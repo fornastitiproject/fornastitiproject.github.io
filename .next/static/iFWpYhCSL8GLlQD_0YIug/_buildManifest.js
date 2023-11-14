a8c3c11675e526394ef0783e195d4c71a8633625
endyarfian <endyarfian94@gmail.com>
2023-11-14 15:54:17 +0700

    Second

---
+++ .next/static/iFWpYhCSL8GLlQD_0YIug/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/89-d6d809b820559655.js","static/chunks/pages/index-3eace8b23e787f26.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
